a55c6ef5e0aeb994fdb5510a1d7f2953d2477692
Benjamin Chaix <ben.chaix@gmail.com>
2016-12-14 18:04:06 +0000

    Added BillTest and Bill classes

---
+++ src/main/java/com/medihealth/billing/model/Bill.java
@@ -0,0 +1,34 @@
+package com.medihealth.billing.model;
+
+import java.util.List;
+
+public class Bill {
+
+	private long id;
+	private Patient patient;
+	private List<Integer> receivedServices;
+	private int vaccineNumber = 0;
+
+	public Bill(long id, Patient patient, List<Integer> receivedServices, int vaccineNumber) {
+		this.id = id;
+		this.patient = patient;
+		this.receivedServices = receivedServices;
+		this.vaccineNumber = vaccineNumber;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public List<Integer> getReceivedServices() {
+		return receivedServices;
+	}
+
+	public int getVaccineNumber() {
+		return vaccineNumber;
+	}
+}
---
+++ src/test/java/com/medihealth/billing/model/BillTest.java
@@ -0,0 +1,57 @@
+package com.medihealth.billing.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BillTest {
+
+	public static final long BILL_ID = 1;
+	public static final int VACCINE_NUMBER = 0;
+	Bill bill;
+
+	@Before
+	public void setUp() throws Exception {
+		// create a new patient
+		Patient patient = new Patient(PatientTest.PATIENT_ID, PatientTest.PATIENT_FIRST_NAME,
+				PatientTest.PATIENT_LAST_NAME, PatientTest.PATIENT_AGE, PatientTest.PATIENT_HAS_INSURANCE);
+
+		// create a list of received services
+		List<Integer> receivedServices = new ArrayList<>();
+		receivedServices.add(1); // diagnosis service
+		receivedServices.add(4); // ECG service
+
+		// create the bill
+		bill = new Bill(BILL_ID, patient, receivedServices, VACCINE_NUMBER);
+	}
+
+	@Test
+	public void whenRetrievingBillPatient() {
+		Patient result = bill.getPatient();
+		Assert.assertNotNull(result);
+	}
+
+	@Test
+	public void whenRetrievingBillReceivedServices() {
+		List<Integer> result = bill.getReceivedServices();
+		Assert.assertEquals(result.size(), 2);
+	}
+
+	@Test
+	public void whenRetrievingBillPatientFirstName() {
+		Assert.assertEquals(PatientTest.PATIENT_FIRST_NAME, bill.getPatient().getFirstName());
+	}
+
+	@Test
+	public void whenRetrievingBillFirstReceivedServideId() {
+		Assert.assertEquals(1, bill.getReceivedServices().get(0).intValue());
+	}
+
+	@Test
+	public void whenRetrievingBillVaccineNumber() {
+		Assert.assertEquals(VACCINE_NUMBER, bill.getVaccineNumber());
+	}
+}
