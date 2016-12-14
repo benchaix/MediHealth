41020af8c5dc17e5ff39f1dde1db5c90cb79cf81
Benjamin Chaix <ben.chaix@gmail.com>
2016-12-14 17:43:53 +0000

    Added PatientTest

---
+++ src/test/java/com/medihealth/billing/model/PatientTest.java
@@ -0,0 +1,25 @@
+package com.medihealth.billing.model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PatientTest {
+	private Patient patient;
+
+	public static final long PATIENT_ID = 1;
+	public static final String PATIENT_FIRST_NAME = "Connor";
+	public static final String PATIENT_LAST_NAME = "MacLeod";
+	public static final int PATIENT_AGE = 498;
+	public static final boolean PATIENT_HAS_INSURANCE = false;
+
+	@Before
+	public void setUp() throws Exception {
+		patient = new Patient(PATIENT_ID, PATIENT_FIRST_NAME, PATIENT_LAST_NAME, PATIENT_AGE, PATIENT_HAS_INSURANCE);
+	}
+
+	@Test
+	public void whenGettingPatient1FullName() {
+		Assert.assertEquals(PATIENT_FIRST_NAME + " " + PATIENT_LAST_NAME, patient.getFullName());
+	}
+}
